e6265527edd808ff0274ecb14616391c7f35bd6f
Jara2392 <daniel.jaramillog@udea.edu.co>
2016-11-15 19:41:05 -0500

    RGB-Challenge full working on rails, best 10 scores are shown after user loss

--- app/controllers/competitors_controller.rb
+++ app/controllers/competitors_controller.rb
@@ -1,4 +1,25 @@
 class CompetitorsController < ApplicationController
-  def index
+  def create
+  	email = params[:competitor][:email]
+  	email = params[:competitor][:score].to_i
+  	competitor = Competitor.find_by_email(email)
+
+  	if competitor && competitor.score < score
+  		competitor.update(competitor_params) if competitor.score < score
+  	else
+  		Competitor.create(competitor_params)
+  	end
+
+  	redirect_to competitors_path
   end
+
+  def index
+  	@competitors = Competitor.all.order(score: :desc).limit(10)
+  end
+
+  private
+
+  	def competitor_params
+  		params.require(:competitor).permit(:email,:score)
+  	end
 end
